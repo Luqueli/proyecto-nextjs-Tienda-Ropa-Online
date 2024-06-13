5447dffa888e8ef73e58a85f82210ee743503d91
Tomás Cueto <tomasco98@gmail.com>
2024-06-13 16:38:07 -0300

    Fixed useFormState

--- app/ui/products/create-form.tsx
+++ app/ui/products/create-form.tsx
@@ -31,6 +31,7 @@ export default function CreateForm({
             className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm dark:bg-gray-950 dark:border-gray-700 dark:text-gray-50"
           />
         </div>
+
         <div id="productname-error" aria-live="polite" aria-atomic="true">
           {state.errors?.productName &&
             state.errors.productName.map((error: string) => (
@@ -39,10 +40,11 @@ export default function CreateForm({
               </p>
             ))}
         </div>
+
       </div>
 
       <div>
-        <label htmlFor="name" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+        <label htmlFor="price" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
             Precio
         </label>
         <div className="mt-1">
@@ -54,6 +56,15 @@ export default function CreateForm({
             className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm dark:bg-gray-950 dark:border-gray-700 dark:text-gray-50"
           />
         </div>
+        <div id="price-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.price &&
+                state.errors.price.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))
+              }
+        </div>
       </div>
 
       <div>
@@ -61,18 +72,29 @@ export default function CreateForm({
           Marca
         </label>
         <div className="mt-1">
-          <Select name="brandName">
+          <select 
+            id="brandName"
+            name="brandName">
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Selecciona una marca" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 {brands.map( (brand) => (
-                  <SelectItem key={brand.name} value={brand.name}>{brand.name}</SelectItem>
+                  <SelectItem value={brand.name}>{brand.name}</SelectItem>
                 ))}
               </SelectGroup>
             </SelectContent>
-          </Select>
+          </select>
+        </div>
+        <div id="brand-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.brandName &&
+                state.errors.brandName.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))
+              }
         </div>
       </div>
       
@@ -81,29 +103,41 @@ export default function CreateForm({
           Categoría
         </label>
         <div className="mt-1">
-          <Select name = "categoryName">
+          <select
+            id="categoryName"
+            name="categoryName">
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Selecciona una categoría" />
             </SelectTrigger>
             <SelectContent>
             <SelectGroup>
                 {categories.map( (category) => (
-                  <SelectItem key={category.name} value={category.name}>{category.name}</SelectItem>
+                  <SelectItem value={category.name}>{category.name}</SelectItem>
                 ))}
               </SelectGroup>
             </SelectContent>
-          </Select>
+          </select>
         </div>
+
+        <div id="category-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.categoryName &&
+                state.errors.categoryName.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))
+              }
+        </div>
+
       </div>
       <div>
         <label htmlFor="description" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
           Descripción
         </label>
         <div className="mt-1">
-          <textarea
+          <input
             id="description"
             name="description"
-            rows={3}
             className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm dark:bg-gray-950 dark:border-gray-700 dark:text-gray-50"
           />
         </div>
