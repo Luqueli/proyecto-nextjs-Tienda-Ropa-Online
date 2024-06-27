1034cee7f113ffe52974b98472b03f22695e466b
Lucas Guillermo Bonetto <lucas98.bonetto@gmail.com>
2024-06-27 20:08:22 -0300

    Prueba recepcion de Post MP

--- app/api/mp_payments/route.ts
+++ app/api/mp_payments/route.ts
@@ -1,7 +1,17 @@
+import {MercadoPagoConfig, Payment} from "mercadopago";
 import { NextRequest } from "next/server";
 
+
 export async function POST(req: NextRequest) {
-    const body = await req.json();
+    const client = new MercadoPagoConfig({accessToken : process.env.MP_ACCESS_TOKEN!});
+    const body = await req.json().then((data) => data as {data : {id : string}});
+    const secret = req.headers.get("x-signature");
+
+    if (secret !== process.env.SECRET_PAYMENT_MP){
+        return Response.json({success:  false});
+    }
+    const payment= await new Payment(client).get({id :body.data.id});
+    
     console.log("body :", body);
     return Response.json({success: true});
 }
