1553aa1d54834e8a9d7614bf9d10101dd838a3f0
Lucas Bonetto <54374762+Luqueli@users.noreply.github.com>
2024-06-27 11:11:46 -0300

    Redireccion de T D N A al carrousel.tsx

--- app/ui/home/navbar.tsx
+++ app/ui/home/navbar.tsx
@@ -9,7 +9,9 @@ export default async function Navbar(){
     return(
         <div className="navbar bg-customCream">
             <div className="navbar-start">
-                <a className="btn btn-ghost text-xl font-bold tracking-widest ml-10">T N D A.</a>
+                <Link href={"/"}>
+                    <div className="btn btn-ghost text-xl font-bold tracking-widest ml-10">T N D A.</div>
+                </Link>
             </div>
 
             <div className="navbar-center hidden lg:flex font-bold">
