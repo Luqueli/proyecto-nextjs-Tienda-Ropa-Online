1a677f3805bbc48fcef0b8b283f462024b6bd210
Lucas Bonetto <54374762+Luqueli@users.noreply.github.com>
2024-06-28 09:31:22 -0300

    Pruebas mp.ts

--- app/api/mp_payments/route.ts
+++ app/api/mp_payments/route.ts
@@ -13,6 +13,6 @@ export async function POST(req: NextRequest) {
     */
     const payment= await new Payment(client).get({id :body.data.id});
 
-    console.log("body :", body);
+    //console.log("body :", body);
     return Response.json({success: true});
 }
