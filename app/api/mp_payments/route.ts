5e4fee95e183cd1aaad5c1ed2e40f940899d9d69
Lucas Bonetto <54374762+Luqueli@users.noreply.github.com>
2024-06-28 09:46:35 -0300

    Pruebas mp

--- app/api/mp_payments/route.ts
+++ app/api/mp_payments/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
     const client = new MercadoPagoConfig({accessToken : process.env.MP_ACCESS_TOKEN!});
     const body = await req.json().then((data) => data as {data : {id : string}});
-    //const secret = req.headers.get("x-signature");
+    const secret = req.headers.get("x-signature");
     /*
     if (secret !== process.env.SECRET_PAYMENT_MP){
         return Response.json({success:  false});
